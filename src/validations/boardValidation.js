418ced2c224cb7ce616897428597f9d55f0e0f4c
La Đại Lộc <ladailoc9a3@gmail.com>
2025-06-15 22:46:01 +0700

    validate data with Joi

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,37 @@
+import Joi from "joi";
+import { StatusCodes } from "http-status-codes";
+
+const createNew = async (req, res, next) => {
+  // Việc validate dữ liệu BẮT BUỘC phải có ở Backend vì đây là điểm cuối để lưu trữ dữ liệu vào Database.
+  // Thông thường trong thực tế, điều tốt nhất cho hệ thống là hãy luôn validate dữ liệu ở cả Frontend và Backend.
+
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      "any.required": "Title is required",
+      "string.empty": "Title is not allowed to be empty",
+      "string.min": "Title must be at least 3 characters long",
+      "string.max": "Title must not exceed 50 characters",
+      "string.trim": "Title must not contain leading or trailing spaces",
+    }),
+    description: Joi.string().required().min(3).max(256).trim().strict(),
+  });
+
+  try {
+    // console.log("Request body:", req.body);
+    await correctCondition.validateAsync(req.body, {
+      abortEarly: false, // Validate all errors, not just the first one
+    });
+
+    // next(); // Proceed to the next middleware or route handler
+
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: "Board created successfully" });
+  } catch (error) {
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message,
+    });
+  }
+};
+
+export const boardValidation = { createNew };
