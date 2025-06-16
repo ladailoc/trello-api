9e4da134175485cb233d697d7f5537af44fc07af
La Đại Lộc <ladailoc9a3@gmail.com>
2025-06-16 23:51:20 +0700

    error_handle

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -2,16 +2,16 @@ import { StatusCodes } from "http-status-codes";
 
 const createNew = async (req, res, next) => {
   try {
-    console.log("Request body:", req.body);
-    console.log("Request query:", req.query);
-    console.log("Request params:", req.params);
+    // console.log("Request body:", req.body);
+    // console.log("Request query:", req.query);
+    // console.log("Request params:", req.params);
     res
       .status(StatusCodes.CREATED)
       .json({ message: "Board created successfully" });
+
+    // throw new ApiError(StatusCodes.BAD_REQUEST, "la dai loc test err");
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message,
-    });
+    next(error);
   }
 };
 
